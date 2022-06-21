d85ecffd328e03a3165398dd624b84deee0d8d51
Lazaro21 <olsf21@gmail.com>
2022-06-21 16:55:41 -0300

    Second commit

--- 6-Java/intro-a-ide/primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ 6-Java/intro-a-ide/primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -3,14 +3,65 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
 
         System.out.println(gato);
 
+        Livro livro = new Livro("Arco Íres da gravidade", 712 );
+        System.out.println(livro);
 /*      int a = 5;
         int b = 3;
         System.out.println("Hello World " + (a + b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return nome.equals(livro.nome) && npag.equals(livro.npag);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, npag);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
     }
 }
